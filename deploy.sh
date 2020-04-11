b59bd34c3a116bd2fb8f409b3f59607489457900
superjmm <673224116@qq.com>
2020-04-11 16:41:58 +0800

    update applicationContext.xml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
 # 输入你的环境上tomcat的全路径
-# export TOMCAT_APP_PATH=tomcat在部署机器上的路径
+# export TOMCAT_HOME=tomcat在部署机器上的路径
 
 ### base 函数
 killTomcat()
@@ -27,18 +27,18 @@ mvn clean install
 killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -rf TOMCAT_HOME/webapps/ROOT
+rm -f TOMCAT_HOME/webapps/ROOT.war
+rm -f TOMCAT_HOME/webapps/order.war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/order/target/order.war TOMCAT_HOME/webapps/
 
-cd $TOMCAT_APP_PATH/webapps/
+cd TOMCAT_HOME/webapps/
 mv order.war ROOT.war
 
 # 启动Tomcat
-cd $TOMCAT_APP_PATH/
+cd TOMCAT_HOME/
 sh bin/startup.sh
 
 
